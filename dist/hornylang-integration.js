1e583de28a0c5b17e573796338662722d00f9c5c
Lia Va <33329419+MiloLug@users.noreply.github.com>
2021-08-06 15:59:39 +0300

    revert cdn check

--- dist/hornylang-integration.js
+++ dist/hornylang-integration.js
@@ -1,1 +1,1 @@
-check cdn
+(()=>{"use strict";document.querySelectorAll('script[type="application/x-hornylang"], script[lang="hornylang"]').forEach((n=>{window.hornylang.Global.eval(n.textContent)}))})();
