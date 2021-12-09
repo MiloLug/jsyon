7e0453a15f8239b230faaf39f10e06a266652582
Lia Va <mistickusya.2012@gmail.com>
2021-12-10 01:30:51 +0200

    optimizations

---
+++ src/native/debug/main.js
@@ -0,0 +1,6 @@
+const Interpreter = require('./interpreter.js');
+const Parser = require('../../parser.js');
+const Global = require('./global.js');
+
+// Just import all lib parts
+module.exports = {Interpreter, Parser, Global};
